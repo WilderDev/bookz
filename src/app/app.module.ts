79ab835d9ba8a5e35de83bc185fb08a67446fdd3
WilderDev <williamjohnwilder@outlook.com>
2022-05-12 12:27:14 -0500

    ADD: Angular Animations && PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,10 +11,24 @@ import { BookshelfModule } from './bookshelf/bookshelf.module';
 import { LibraryModule } from './library/library.module';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SharedModule } from './shared/shared.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
--- src/app/library/book-results/book-results.component.ts
+++ src/app/library/book-results/book-results.component.ts
@@ -1,12 +1,33 @@
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+
+import { BookshelfService } from '../../bookshelf/bookshelf.service';
 import { Book } from '../../shared/book/book.model';
 import { LibraryService } from '../library.service';
-import { BookshelfService } from '../../bookshelf/bookshelf.service';
 
 @Component({
-  selector: 'app-book-results',
-  templateUrl: './book-results.component.html',
-  styleUrls: ['./book-results.component.css'],
+  selector: "app-book-results",
+  templateUrl: "./book-results.component.html",
+  styleUrls: ["./book-results.component.css"],
+  animations: [
+    trigger("swoopIn", [
+      transition(":enter", [
+        animate(
+          "1s",
+          keyframes([
+            style({
+              transform: "scale(0)",
+              opacity: 0
+            }),
+            style({
+              transform: "scale(1)",
+              opacity: 1
+            })
+          ])
+        )
+      ])
+    ])
+  ]
 })
 export class BookResultsComponent implements OnInit {
   searchResults: Book[] = [];
